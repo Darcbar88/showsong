02ab36eaa66587f43512ea3c1242f4a1a7a635a2
Darcbar88 <barrydarcy002@aol.co.uk>
2011-01-03 17:01:40 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showsong::Application.config.secret_token = 'c50948762b235ac9ef28b8d745d274ed8e04536691a7044d4a47df3cf4a4d58d5884b8864d2f8004d8e1d71663b5976955fccc44a5cdae8ea82c567224cf5d24'
